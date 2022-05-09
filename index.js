992bbda1d1207f3cb9a3ef9737cb57509d986f63
Hollis Jamison <hollisjamison@gmail.com>
2022-05-09 18:36:08 -0500

    Sets up intial test routes

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const express = require('express')
 
 const app = express()
 
+app.get('/team', (request, response) => {
+    return response.status(200).send("TO DO: /team controller")
+})
+
+app.get('/team/:id', (request, response) => {
+    // const {id} = request.params
+    console.log(request.headers)
+    return response.status(200).send("TO DO: /team/ID controller")
+})
 
 
 app.listen(1300, () => {
