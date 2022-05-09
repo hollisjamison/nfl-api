0282589bdb5c67e96fdd26ca82ea227ae1801694
Hollis Jamison <hollisjamison@gmail.com>
2022-05-09 18:53:16 -0500

    getAllTeams is now functioning

--- controllers/team.js
+++ controllers/team.js
@@ -1,10 +1,12 @@
+const teams = require('../teams')
+
+const getAllTeams = (request, response) => {
+  return response.status(200).send(teams)
+}
+
 const getTeamById = (request, response) => {
   // const {id} = request.params
   return response.status(200).send('IN PROGRESS: /team/ID controller')
-}
-
-const getAllTeams = (request, response) => {
-  return response.status(200).send('TO DO: /team controller')
 }
 
 module.exports = { getTeamById, getAllTeams }
